31c72cf2452d07aa680f2d6104c62dcd851b901a
Marcel Wagenländer <marcel.wagenlander19@imperial.ac.uk>
2024-06-15 12:20:11 +0100

    finish evaluation of todos

--- polish_todos.py
+++ polish_todos.py
@@ -1,3 +1,5 @@
+import json
+
 import cohere
 
 
@@ -7,11 +9,56 @@ def get_api_key():
     return api_key
 
 
+def get_todos():
+    # TODO: query DB
+    todos = [
+        "Buy tomatoes at M&S",
+        "Be in a real relationship by 35",
+        "Complete the intial project proposal",
+        "Finish Cohere internship application",
+    ]
+    return todos
+
+
+def update_todos(todos: list):
+    # TODO: update DB
+    pass
+
+
+def extract_json(text: str):
+    text_lines = text.splitlines()
+    json_lines = []
+    is_json = False
+    for lin in text_lines:
+        if is_json:
+            if lin == "```":
+                break
+            json_lines.append(lin)
+        if lin == "```json":
+            is_json = True
+    json_str = "\n".join(json_lines)
+    json_obj = json.loads(json_str)
+    return json_obj
+
+
 def main():
     api_key = get_api_key()
     coh = cohere.Client(api_key)
-    response = coh.chat(message="Is this API call working?")
-    print(response)
+
+    todos = get_todos()
+    instruction = (
+        "Evaluate the todos based on the SMART criteria"
+        " and if the criteria is not fullfilled"
+        " propose a rephrase of the todo or a split into sub-todos."
+        " Return the new todos in a JSON list."
+    )
+    todo_list = "\n- ".join(todos)
+    message = f"{instruction}\n- {todo_list}"
+    print(message)
+    response = coh.chat(message=message)
+    polished_todos = extract_json(response.text)
+    print(polished_todos)
+    update_todos(polished_todos)
 
 
 if __name__ == "__main__":
